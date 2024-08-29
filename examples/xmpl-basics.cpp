1bb73305aa312ba0b2254dc3f714fa0987397255
krokoreit <krokoreit@gmail.com>
2024-08-29 11:59:21 +0200

    updated basics example

--- examples/xmpl-basics.cpp
+++ examples/xmpl-basics.cpp
@@ -43,20 +43,20 @@ myObject::myObject(std::string text, uint32_t number)
 }
 
 
-// the store
+// the store - no sorting
 spObjectStore<myObject> myObjectStore;
 
 
 /**
- * @brief callback function to print a stored object with key and object members
+ * @brief callback function to print a stored object with id and object members
  * 
- * @param key 
- * @param pObj pointer to myObject object 
+ * @param id 
+ * @param obj a myObject object 
  * @return true (as we do not want to stop iterarion)
  */
-bool printMyObject(std::string key, myObject* pObj)
+bool printStoredObject(const std::string &id, const myObject &obj)
 {
-  printf("Key: %s, text: %s, number: %i\n", key.c_str(), pObj->_text.c_str(), pObj->_number);
+  printf("id: %s, text: %s, number: %i\n", id.c_str(), obj._text.c_str(), obj._number);
   return true;
 }
 
@@ -73,9 +73,25 @@ void printObjectStore()
   }
   printf("object store content: %d objects, %ssorted:\n", myObjectStore.getSize(), sortedTxt.c_str());
   // loop output
-  myObjectStore.forEach(&printMyObject);
+  myObjectStore.forEach(&printStoredObject);
 }
 
+/**
+ * @brief print object at pointer supplied
+ * 
+ * @param pObj  pointer to myObject
+ */
+void printObjAtPointer(const std::string &id, myObject *pObj)
+{
+  if (pObj == nullptr)
+  {
+    printf("printObjAtPointer: object with id %s does not exist\n", id.c_str());
+  }
+  else
+  {
+    printf("printObjAtPointer: object with id: %s, text: %s, number: %i\n", id.c_str(), pObj->_text.c_str(), pObj->_number);
+  }
+}
 
 /**
  * @brief our main function
@@ -87,33 +103,37 @@ int main(int argc, char *argv[])
     printf("running %s\n", a.substr(a.rfind(std::filesystem::path::preferred_separator) + 1).c_str());
 
     // addObj() with implicit object creation
-    myObjectStore.addObj("one", "one", 1);
-    myObjectStore.addObj("two", "two", 2);
-    myObjectStore.addObj("no number", "no numer given");
+    myObjectStore.addObjWithId("one", "one", 1);
+    myObjectStore.addObjWithId("two", "two", 2);
+    myObjectStore.addObjWithId("no number", "no numer given");
 
-    // getObj() with implicit object creation
-    myObject *pObj3 = myObjectStore.getObj("three", true, "three", 3);
-    myObject *pObj4 = myObjectStore.getObj("four", true);
-    pObj4->_text = "text assigned later";
+    // setObj() after separate object creation
+    myObject obj3("three", 3);
+    myObjectStore.setObjWithId("three", obj3);
+    myObject obj4("four");
+    myObject *pObj4 = myObjectStore.setObjWithId("four", obj4);
 
-    // setObj() with existing object
-    myObject obj5("five", 5);
-    myObjectStore.setObj("five", obj5);
+    // note that obj4 is copied, i.e. not the object in store!
+    obj4._text = "text assigned later, but not on object in store";
+    // changing pObj4 is possible until recreation of store, i.e. changed sorting
+    pObj4->_text = "text assigned later on object in store";
 
     // print current state
     printObjectStore();
 
-    myObjectStore.deleteObj("one");
-    myObjectStore.deleteObj("two");
-    myObjectStore.deleteObj("four");
+    myObjectStore.deleteObjById("one");
+    myObjectStore.deleteObjById("two");
 
     // print current state 2
     printf("after deleting objects:\n");
     printObjectStore();
 
-    myObject *pObj6 = myObjectStore.getObj("five");
-
-    myObject *pObj7 = myObjectStore.getObj("not_there");
+    std::string id = "four";
+    myObject *pObj6 = myObjectStore.getObjById(id);
+    printObjAtPointer(id, pObj6);
+    id = "not_there";
+    myObject *pObj7 = myObjectStore.getObjById(id);
+    printObjAtPointer(id, pObj7);
 
     printf("done\n");
 }
